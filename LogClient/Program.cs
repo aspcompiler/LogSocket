48daae52bede3f2aa9c4088d446d1d69bda9da96
dotneteer <lichenca@hotmail.com>
2017-05-13 22:12:49 -0700

    Add project files.

---
+++ LogClient/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client;
+
+namespace LogClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync(args).Wait();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            //Waiting for the web server to start
+            await Task.Delay(10000);
+
+            var hubConnection = new HubConnection("http://localhost:56042/");
+            IHubProxy socketHub = hubConnection.CreateHubProxy("Socket");
+            await hubConnection.Start();
+
+            var observable = Observable.Timer(TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1));
+            observable.Subscribe(l =>
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    socketHub.Invoke("WriteLog", $"Stream{i}", $"Stream{i} {DateTime.Now}");
+                }
+            });
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LogSocket/Hubs/Socket.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace LogSocket.Hubs
+{
+    public class Socket : Hub
+    {
+        public static HashSet<string> Subscribers = new HashSet<string>();
+
+        public async Task Subscribe(string stream)
+        {
+            await Groups.Add(Context.ConnectionId, stream);
+        }
+
+        public async Task Unsubscribe(string stream)
+        {
+            await Groups.Remove(Context.ConnectionId, stream);
+        }
+
+        public void WriteLog(string stream, string message)
+        {
+            Subscribers.Add(stream);
+            Clients.Group(stream).addMessage(message);
+        }
+    }
+}
